db68ae8330bd81c3261ae912f49004accbf3b59f
Trepte <mtrepte@SDGL152a9fc77.local>
2017-07-28 15:21:23 -0700

    added PCA imgs

--- src/models/pca_plot.py
+++ src/models/pca_plot.py
@@ -21,10 +21,10 @@ def review_len_analysis():
                 text_sum += len(review['text'])
                 count += 1
 
-        avg_title_len = str(title_sum // count)[:4]
-        avg_pros_len = str(pros_sum // count)[:4]
-        avg_cons_len = str(cons_sum // count)[:4]
-        avg_text_len = str(text_sum // count)[:4]
+        avg_title_len = str(title_sum / count)[:4]
+        avg_pros_len = str(pros_sum / count)[:4]
+        avg_cons_len = str(cons_sum / count)[:4]
+        avg_text_len = str(text_sum / count)[:4]
 
         print("rating:", i, "count:", count, "avg_title_len:", avg_title_len, "avg_pros_len:", avg_pros_len, "avg_cons_len:", avg_cons_len, "avg_text_len:", avg_text_len)
     print()
@@ -42,15 +42,15 @@ transcripts = []
 ratings = []
 for review in reviews:
     rating = int(review['rating'])
-    ratings.append(rating)
+    if rating <= 5:
+        ratings.append(rating)
 
-    transcript = review['text']
-    for con in review['cons']:
-        transcript += ' ' + con
-    transcripts.append(transcript)
+        transcript = review['text']
+        for con in review['cons']:
+            transcript += ' ' + con
+        transcripts.append(transcript)
 
-
-vectorizers = [CountVectorizer(), TfidfVectorizer()]
+vectorizers = [TfidfVectorizer()]
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
 
 for vectorizer in vectorizers:
@@ -58,7 +58,7 @@ for vectorizer in vectorizers:
     # vectorize
     X = vectorizer.fit_transform(transcripts)
     X = X.toarray()
- 
+
     # PCA
     X = PCA(n_components=2).fit_transform(X)
 
@@ -66,7 +66,7 @@ for vectorizer in vectorizers:
     for i in range(len(X)):
         x, y = X[i, 0], X[i, 1]
         rating = ratings[i]
-        plt.scatter(x, y, c=colors[rating - 1], cmap=plt.cm.Paired)
+        plt.scatter(x, y, c=colors[rating - 1], cmap=plt.cm.Paired, s = 4)
 
     # patches = [mpatches.Patch(color=colors[i], label=labels[i]) for i in range(len(labels))]
     # plt.legend(handles=patches, loc=locs[j], fontsize=10)
