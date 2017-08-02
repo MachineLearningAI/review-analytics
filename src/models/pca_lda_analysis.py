bc0be40c5d32cf7bb0c55f3673834d32be397fd2
Andrew Li <andrewyli@gmail.com>
2017-08-02 14:46:05 -0700

    including n-gram analysis and increasing iteration count

--- src/models/pca_lda_analysis.py
+++ src/models/pca_lda_analysis.py
@@ -1,6 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation as LDA
 from sklearn.decomposition import PCA
+from nltk.corpus import stopwords
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -9,26 +10,29 @@ import numpy as np
 from time import time
 
 # the highest rating to consider reviews for
-RATING_THRESHOLD = 5
+# DO NOT MAKE MAX_ITER HIGH WHEN RATING_THRESHOLD IS 5
+RATING_THRESHOLD = 4
 METHOD_NAME = "lda"
-NUM_TOPICS = 8 # LDA
+NUM_TOPICS = 5 # LDA
 NUM_COMPONENTS = 2 # PCA
 LEARNING_DECAY = 0.7 # should be in the interval (0.5, 1.0]
-MAX_ITER = 10
+MAX_ITER = 20
 NUM_TOP_WORDS = 20
 
 
 # http://scikit-learn.org/stable/auto_examples/applications/topics_extraction_with_nmf_lda.html
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
-        print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
+        print("Topic #%d:" % (topic_idx + 1))
+        print(" ".join([feature_names[i] # + "," + str(topic[i])
                         for i in topic.argsort()[:-n_top_words - 1:-1]]))
     print()
+    return [(feature_names[i], topic[i])
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]
 
 
 reviews_array = []
-review_text = open('../data/reviews.json', 'r').read()
+review_text = open('reviews.json', 'r').read()
 reviews = eval(review_text)
 
 # create transcripts and ratings
@@ -44,11 +48,31 @@ for review in reviews:
             transcript += ' ' + con
         transcripts.append(transcript)
 
-stop_words = ["to", "the", "and", "it", "was", "for", "my", "is", "of", "that", "you", "me", "tax", "turbo", "turbotax", "taxes", "in", "as", "do", "with", "at", "be", "your", "on", "this", "its", "use", "but", "all", "have", "there", "had", "so", "or", "if", "will", "way", "can", "get", "which", "did", "very", "don", "too", "an", "been", "when", "didn", "would", "ve", "they", "file", "filing", "year", "out", "from", "by", "how", "am"]
-optional_stop_words = ["easy", "not", "just", "much", "many", "only", "up", "using", "used", "free", "some", "always", "every", "years", "are", "really", "what", "than", "none", "no"]
-stop_words.extend(optional_stop_words)
+# stop_words = ["to", "the", "and", "it", "was", "for", "my", "is", "of", "that", "you", "me", "tax", "turbo", "turbotax", "taxes", "in", "as", "do", "with", "at", "be", "your", "on", "this", "its", "use", "but", "all", "have", "there", "had", "so", "or", "if", "will", "way", "can", "get", "which", "did", "very", "don", "too", "an", "been", "when", "didn", "would", "ve", "they", "file", "filing", "year", "out", "from", "by", "how", "am"]
+# optional_stop_words = ["just", "much", "many", "up", "using", "used", "some", "always", "every", "years", "are", "really", "what", "than", "doing", "going"]
+# stop_words.extend(optional_stop_words)
 
-vectorizers = [TfidfVectorizer(stop_words=stop_words)]
+
+
+# Interesting sets of stopwords:
+# using nltk
+
+# topics: 6, iter: 20
+# stop_words = stopwords.words("english")
+# stop_words.extend(["tax", "taxes", "turbo", "turbotax"])
+
+# topics: 6, iter: 20
+# stop_words = stopwords.words("english")
+# stop_words.extend(["tax", "taxes", "turbo", "turbotax", "easy"])
+
+# topics: 5, iter: 20
+stop_words = stopwords.words("english")
+stop_words.extend(["tax", "taxes", "turbo", "turbotax", "easy"])
+
+
+
+
+vectorizers = [TfidfVectorizer(stop_words=stop_words, ngram_range=(1, 3))]
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
 
 for vectorizer in vectorizers:
@@ -75,8 +99,9 @@ for vectorizer in vectorizers:
                   doc_topic_prior=50 / NUM_TOPICS,
                   topic_word_prior=0.1,
                   learning_decay=LEARNING_DECAY,
+                  max_iter=MAX_ITER
         )
         t0 = time()
         lda.fit_transform(X)
         print("lda fit done in %0.3fs" % (time() - t0))
-        print_top_words(lda, vectorizer.get_feature_names(), NUM_TOP_WORDS)
+        word_weight_list = print_top_words(lda, vectorizer.get_feature_names(), NUM_TOP_WORDS)
